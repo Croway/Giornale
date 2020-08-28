7e34633474adb4789d438df920b2d6b164acdf68
Mariani Federico <federico.mariani.1990@gmail.com>
2020-08-28 12:02:03 +0200

    integration test

--- giornale-it/src/test/java/it/croway/giornale/GiornaleIT.java
+++ giornale-it/src/test/java/it/croway/giornale/GiornaleIT.java
@@ -13,7 +13,7 @@ class GiornaleIT {
 
 	@Test
 	void baseTest() {
-		ResponseEntity re = restTemplate.getForEntity("http://localhost:8080/notizie", ResponseEntity.class);
+		ResponseEntity re = restTemplate.getForEntity("http://localhost:8081/notizie", ResponseEntity.class);
 		
 		Assertions.assertEquals(200, re.getStatusCodeValue());
 	}
